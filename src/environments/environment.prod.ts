e15256463c986c13018d2b32b7e6ab8cc1343c25
xeontem EPAM <xeontem@gmail.com>
2017-09-15 18:30:34 +0300

    firebase/fix: heroku test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDtWmHBfmDSMu8slmlMHrjdMSekwGEn9GQ",
+    authDomain: "pizzatwilio.firebaseapp.com",
+    databaseURL: "https://pizzatwilio.firebaseio.com",
+    projectId: "pizzatwilio",
+    storageBucket: "pizzatwilio.appspot.com",
+    messagingSenderId: "603360306030"
+  }
 };
